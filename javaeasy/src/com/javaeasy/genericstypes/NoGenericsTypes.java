f3f035b35c61fa0bc537670cc31298084f6927e5
karlinglee93 <459607025@qq.com>
2018-07-11 16:15:52 +0800

    GenericsTypes

--- javaeasy/src/com/javaeasy/genericstypes/NoGenericsTypes.java
+++ javaeasy/src/com/javaeasy/genericstypes/NoGenericsTypes.java
@@ -8,10 +8,12 @@ public class NoGenericsTypes {
 
 	public static void main(String[] args) {	// main()方法
 		Student stu = new Student("刘伟", 1);	// 创建一个Stusdent类的实例
-		List students = new ArrayList();		// 创建一个ArrayList的实例
-		students.add(stu);						// 将stu放入List中
-		Student stuFromList = (Student) students.get(0);
-												// 得到第一个元素，强制类型转换
+		// (1) 创建一个ArrayList 的实例，尖括号中的内容是指定此列表中元素的类型
+		List<Student> students = new ArrayList<Student>();
+		// (2) 将stu 放入List 中，此时students 只能够添加student 类型的元素
+		students.add(stu);
+		// (3) 得到第一个元素，因为使用了泛型，所以无需强制类型转换
+		Student stuFromList = students.get(0);
 		printStudentProps(stuFromList);		// 传递给printStudent-
 												// Props()方法
 	}
