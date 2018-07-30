05e13e7840ce56babf448e307231ec9744b01062
karlinglee93 <459607025@qq.com>
2018-07-30 14:58:51 +0800

    MavenDemo of JavaWeb

--- MavenDemo/src/main/java/com/safari/MavenDemo/App.java
+++ MavenDemo/src/main/java/com/safari/MavenDemo/App.java
@@ -1,13 +1,11 @@
 package com.safari.MavenDemo;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws ClassNotFoundException
     {
+    	Class.forName("com.mysql.jdbc.Driver");
+    	System.out.println("初始化驱动成功");
         System.out.println( "Hello World!" );
     }
 }
