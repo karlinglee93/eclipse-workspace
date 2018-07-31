9fccc87c97aeb7d5590f2383c80ac1294b08c1a2
karlinglee93 <459607025@qq.com>
2018-07-31 19:50:54 +0800

    Session check/clear

--- springmvc_test/src/controller/IndexController.java
+++ springmvc_test/src/controller/IndexController.java
@@ -2,6 +2,7 @@ package controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +21,32 @@ public class IndexController {
 	@RequestMapping("/hello")
 	public ModelAndView jump() {
 		ModelAndView mav = new ModelAndView("redirect:/index");
+		return mav;
+	}
+
+	@RequestMapping("/check")
+	public ModelAndView check(HttpSession session) {
+		Integer i = (Integer) session.getAttribute("count");
+		if (i == null) {
+			i = 0;
+		}
+		i++;
+		session.setAttribute("count", i);
+		ModelAndView mav = new ModelAndView("check");
+
+		return mav;
+	}
+
+	@RequestMapping("/clear")
+	public ModelAndView clear(HttpSession session) {
+		Integer i = (Integer) session.getAttribute("count");
+		ModelAndView mav;
+		if (i == null) {
+			mav = new ModelAndView("redirect:/check");
+		}
+		session.removeAttribute("count");
+		mav = new ModelAndView("check");
+
 		return mav;
 	}
 }
