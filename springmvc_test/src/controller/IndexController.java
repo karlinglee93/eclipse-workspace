1ac9d0416290bdf37e66b3b36097930f725c2995
karlinglee93 <459607025@qq.com>
2018-08-01 13:54:34 +0800

    update format

--- springmvc_test/src/controller/IndexController.java
+++ springmvc_test/src/controller/IndexController.java
@@ -6,7 +6,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
-public class IndexController implements Controller{
+public class IndexController implements Controller {
 	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		ModelAndView mav = new ModelAndView("index");
 		mav.addObject("message", "Hello Spring MVC");
